506206a69f20e4f599acc3512f5eb207796e65cf
Melichoux <meli.tournerie@gmail.com>
2025-12-23 14:09:06 +0100

    header ok, footer commencé, merge avec main

---
+++ Assets/Javascript/index.js
@@ -0,0 +1,6 @@
+// Récupération des données des articles pour la page catalogue
+
+fetch("http://127.0.0.1:5500/Assets/Javascript/Data/articles.json")
+.then((response) => response.json())
+.then(
+    (data)=>{})
