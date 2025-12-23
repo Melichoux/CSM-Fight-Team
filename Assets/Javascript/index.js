7cac967921927b6249ce90fd0c1677c323cc05c9
Melichoux <meli.tournerie@gmail.com>
2025-12-23 16:52:31 +0100

    Creation du catalogue avec fetch + debut CSS

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -3,4 +3,20 @@
 fetch("http://127.0.0.1:5500/Assets/Javascript/Data/articles.json")
 .then((response) => response.json())
 .then(
-    (data)=>{})
+    (data)=>{
+        for (let index = 0; index < data.length; index++) {
+            const articleContainer = document.querySelector(".article-container");
+            const element = data[index];
+            
+            const article = document.createElement("article")
+            article.innerHTML= `
+            <p> ${element.date} </p>
+            <img src="${element.img}" alt = "Photo de l'événement">
+            <h2> ${element.titre}
+            <p> ${element.intro} </p>
+            <a href="article.html"
+            `
+            articleContainer.appendChild(article)
+
+        }
+    })
