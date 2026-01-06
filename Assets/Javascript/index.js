0315c4c9edbfd7cdf8e13590af7c76079c36af44
Melichoux <meli.tournerie@gmail.com>
2026-01-06 16:26:02 +0100

    Fetch avec script dans le fichier article

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -1,6 +1,5 @@
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-  // Commande du menu deroulant sur l'onglet "Vie du club"
+  //ANCHOR - Commande du menu deroulant sur l'onglet "Vie du club"
         const dropDownBtn = document.querySelector("#dropDownBtn");
         const dropDownList = document.querySelector("#dropDownList");
         
@@ -8,44 +7,45 @@
             event.stopPropagation(); // empêche le clic de "remonter" au document et de faire disparaitre le menu tout de suite apres le "click"
             dropDownList.classList.toggle("open")});
 
-  // fermer le menu si on clique ailleurs
+  //SECTION - fermer le menu si on clique ailleurs
   document.addEventListener("click", () => {
     dropDownList.classList.remove("open");
   });
   
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-  // Récupération des données des articles pour la page catalogue
+  //ANCHOR -   Récupération des données des articles pour la page catalogue
 
 fetch("http://127.0.0.1:5500/assets/javascript/data/articles.json")
 .then((response) => response.json())
 .then(
     (data)=>{
+      const articleContainer = document.querySelector(".article-container");
+      if (articleContainer) {
         for (let index = 0; index < data.length; index++) {
-            const articleContainer = document.querySelector(".article-container");
             const element = data[index];
-            
+
             const article = document.createElement("article")
             article.innerHTML= `
             <img src="${element.img}" alt = "Photo de l'événement">
             <p class="dateCard"> ${element.date} </p>
             <h2 class="titreCard"> ${element.titre}
             <p class="introCard"> ${element.intro} </p>
-            <a href="article.html?id=${element.id}" class="btnCard">En savoir +</a>
+            <a href="article.html?id=${element.id}" id="btnCard" class="btnCard">En savoir +</a>
             `
-            articleContainer.appendChild(article)
-
-        }
-    })
+            articleContainer.append(article) // preferer append à appendChild (cf.mdn)
+          }}})
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  //SECTION -  Récupération des données pour la page article
+  //     const submitEvt = document.querySelector("#submit")
+  //     else if (articleContainer && submitEvt) {
+  //         const btnCard = document.querySelector("#btnCard")
+  // }
 
-  // Récupération des données pour la page article
 
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-  // Page catalogue: barre de filtre 
+  //ANCHOR -  Page catalogue: barre de filtre 
   const formFilter = document.querySelector("#formFilter")
   formFilter.addEventListener("submit", (event) => {
     if (event.value = value) {
